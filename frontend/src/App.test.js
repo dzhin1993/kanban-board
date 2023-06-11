1c6bd4d47b8a7a05ba5c1a24a42256e948632ff2
izhynko <zhinkoilya1993@gmail.com>
2023-06-11 11:38:23 +0200

    init tests front

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,19 @@
-import { render, screen } from '@testing-library/react';
+import React from "react";
+import {Provider} from "react-redux";
+import {render, screen} from '@testing-library/react';
+
+
 import App from './App';
+import store from "./store";
+
+
+const renderApp = () =>
+    <Provider store={store}>
+      <App/>
+    </Provider>
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+    render(renderApp());
+    const linkElement = screen.getByText(/Kanban Board/i);
+    expect(linkElement).toBeInTheDocument();
 });
